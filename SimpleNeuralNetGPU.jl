168837198b04800d5738bc363e3f8e362792e5bb
Wanyue <RosettaWYzhang@users.noreply.github.com>
2017-11-19 11:56:15 +0000

    use inbuilt sigma in NNlib

--- SimpleNeuralNetGPU.jl
+++ SimpleNeuralNetGPU.jl
@@ -25,19 +25,20 @@ N = 505 # number of training points
 D = 13 # dimension of each x vector
 hidden_units = 20
 learning_rate = 0.1
-iterations = 5000
+iterations = 20000
 
-hiddenLayer = Dense(D, hidden_units, sigma)
+hiddenLayer = Dense(D, hidden_units, σ)
 outputLayer = Dense(hidden_units,1)
 model = Chain(hiddenLayer, outputLayer)
 
 #for GPU support
 #x,y = cu(x), cu(y)
 #m = mapleaves(cu, model)
-data = Iterators.repeated((x,y), 3)
+
+data = Iterators.repeated((x,y), iterations)
 opt = SGD(params(model), learning_rate)
 E(x, y) = mse(model(x), y)
 evalcb = () -> @show(E(x, y))
-sigma = 
+
 
 Flux.train!(E, data, opt, cb = throttle(evalcb, 5))
