c20ee622005bb570099c96140c22e1253cb69462
Wanyue <RosettaWYzhang@users.noreply.github.com>
2018-02-05 17:26:07 +0000

    attempt to run autoencoder on GPU

--- autoencoderMike.jl
+++ autoencoderMike.jl
@@ -8,13 +8,13 @@ using CuArrays
 
 # Encode MNIST images as compressed vectors that can later be decoded back into
 # images.
-
 imgs = MNIST.images()
 N = 60000 # Size of the encoding
 batchSize = 200
 # Partition into batches
+X =  hcat(float.(reshape.(imgs, :))...)
 data = [(float(hcat(vec.(imgs[i])...)),) for i in partition(1:60_000, batchSize)]
-data = cu(data[:][1])
+data = cu.(data)
 # define autoencoder structure: 784-1000-500-250-30
 m = Chain(
   # Encoder
@@ -34,8 +34,9 @@ sigma(x)=1./(1+exp.(-x))
 loss(x) = mse(m(x), x)
 squared_loss_test(x) = sum(sum((x - sigma(m(x))).^2))/batchSize
 log_loss(x) = begin; y=m(x); return -sum( x.*log.(y)+(1-x).*log.(1-y))/batchSize; end
-#wloss(x) = begin; h=m(x); return mean(h.*(1-x)+log.(1+exp.(-h))); end
-wyloss(x) = begin; h=m(x); return mean(-x.*h + log1pexp(h)); end
+# combine sigma function with log loss
+wyloss(x) = begin; h=m(x); return mean(-x.*h + log1pexp.(h)); end
+cuLoss(x) = begin; h=m(x); return mean(-x.*h + CUDAnative.log.(1+exp.(h))); end
 
 
 evalcb = () -> @show squared_loss_test(data[1][1])
@@ -43,9 +44,5 @@ opt = Momentum(params(m),0.1)
 
 @progress for i = 1:10
   info("Epoch $i")
-  Flux.train!(wyloss, data, opt, cb = evalcb)
+  Flux.train!(cuLoss, data, opt, cb = evalcb)
 end
-
-
-#
-#
